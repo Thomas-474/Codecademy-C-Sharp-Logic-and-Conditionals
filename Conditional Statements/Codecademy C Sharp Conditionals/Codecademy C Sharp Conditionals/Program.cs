c4e887072f951ba381d5d7bd53b9add4f33db2d1
Thomas-474 <teg2004@icloud.com>
2021-09-02 10:15:34 -0400

    Add project files.

---
+++ Conditional Statements/Codecademy C Sharp Conditionals/Codecademy C Sharp Conditionals/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codecademy_C_Sharp_Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region If Statements
+                Console.WriteLine("If Statements:");
+
+                Console.WriteLine("Enter number of pairs of socks: ");
+                string socksString = Console.ReadLine();
+                int socksInt = Convert.ToInt32(socksString);
+
+                // string centsAsString = Console.ReadLine();
+                // double centsAsDouble = Convert.ToDouble(centsAsString);
+
+                if (socksInt <= 3)
+                {
+                    Console.WriteLine("Time to do laundry!");
+                }
+
+                if (socksInt > 3)
+                {
+                    Console.WriteLine("Don't do laundry yet.");
+                }
+
+                Console.ReadLine();
+            #endregion
+
+            #region If...Else... Statements
+                Console.WriteLine("\nIf...Else... Statements:");
+
+                // int people = 10;
+                // string weather = "nice";
+
+                int people = 12;
+                string weather = "bad";
+
+                if (people <= 10 && weather == "nice")
+                {
+                    Console.WriteLine("SaladMart");
+                }
+
+                else
+                {
+                    Console.WriteLine("Soup N Sandwich");
+                }
+            #endregion
+
+            #region Else If Statements
+                Console.WriteLine("\nElse If Statements:");
+
+                int guests = 7;
+
+                if (guests >= 4)
+                {
+                    Console.WriteLine("Catan");
+                }
+
+                else if (guests >= 1)
+                {
+                    Console.WriteLine("Innovation");
+                }
+
+                else
+                {
+                    Console.WriteLine("Solitaire");
+                }
+            #endregion
+
+            #region Switch Statements
+                Console.WriteLine("\nSwitch Statements:");
+
+                Console.WriteLine("Pick a genre: ");
+                string genre = Console.ReadLine();
+
+                switch (genre)
+                {
+                    case "Drama":
+                        Console.WriteLine("Citizen Kane");
+                        break;
+                    case "Comedy":
+                        Console.WriteLine("Duck Soup");
+                        break;
+                    case "Adventure":
+                        Console.WriteLine("King Kong");
+                        break;
+                    case "Horror":
+                        Console.WriteLine("Psycho");
+                        break;
+                    case "Science Fiction":
+                        Console.WriteLine("2001: A Space Odyssey");
+                        break;
+                    default:
+                        Console.WriteLine("No movie found");
+                        break;
+                }
+            #endregion
+
+            #region Ternary Operators
+                Console.WriteLine("\nTernary Operators:");
+
+                int pepperLength = 4;
+                string message = (pepperLength >= 3.5) ? "ready!" : "wait a little longer";
+
+                Console.WriteLine(message);
+
+                // string color = "blue";
+                // string result = (color == "blue") ? "blue" : "NOT blue";
+
+                // Console.WriteLine(result);
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Understanding Logic in C#/Codecademy C Sharp Logic/Codecademy C Sharp Logic/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codecademy_C_Sharp_Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Boolean Data Types
+            Console.WriteLine("Boolean Data Types:");
+            bool answerOne = true;
+            bool answerTwo = false;
+            Console.WriteLine($"True or False: The number 500 is greater than 24. {answerOne}");
+            Console.WriteLine($"True or False: “Coffee” contains the letter “a”. {answerTwo}\n");
+
+            // Comparison Operators
+            Console.WriteLine("Comparison Operators:");
+            double timeToDinner = 4;
+            double distance = 95;
+            double rate = 30;
+            double tripDuration = distance / rate;
+            bool answer = tripDuration <= timeToDinner;
+            Console.WriteLine($"You are driving to your family’s house for a holiday and want to see if you’ll get there before dinner. Dinner will begin at 6:00 PM and the house is 95 miles away. If you leave at 2:00PM and drive an average of 30 miles per hour, will you get there early (before 6:00pm)? {answer}\n");
+
+            // Logical Operators
+            Console.WriteLine("Logical Operators:");
+            bool beach = true;
+            bool hiking = false;
+            bool city = true;
+            bool yourNeeds = beach && city;
+            bool friendNeeds = beach && hiking;
+            bool tripDecision = yourNeeds && friendNeeds;
+            Console.WriteLine($"Will you agree on Barcelona? {tripDecision}");
+
+            Console.ReadKey();
+        }
+    }
+}
